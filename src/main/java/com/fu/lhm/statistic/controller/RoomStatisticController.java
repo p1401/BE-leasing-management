e6b7cc5445629767d8addb41694b884be89d82e2
Hoàng IT <ngominhhoang190@gmail.com>
2023-04-07 16:24:23 +0700

    update

--- src/main/java/com/fu/lhm/statistic/controller/RoomStatisticController.java
+++ src/main/java/com/fu/lhm/statistic/controller/RoomStatisticController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/roomStatistic")
+@RequestMapping("api/v1/room-statistic")
 @RequiredArgsConstructor
 public class RoomStatisticController {
 
