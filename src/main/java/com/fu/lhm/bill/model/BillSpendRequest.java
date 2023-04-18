e0cdfc2b39e309f1bc7be7c45f738794592e5e4d
Phuc Nguyen <66867906+p1401@users.noreply.github.com>
2023-04-18 10:24:49 +0700

    update

---
+++ src/main/java/com/fu/lhm/bill/model/BillSpendRequest.java
@@ -0,0 +1,32 @@
+package com.fu.lhm.bill.model;
+
+import com.fu.lhm.bill.entity.BillType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillSpendRequest {
+    private Long id;
+
+    private int roomMoney;
+
+    private String billCode;
+
+    private int totalMoney;
+
+    private Date dateCreate;
+
+    private String description;
+
+    //Phieu thu, phieu chi
+    @Enumerated(EnumType.STRING)
+    private BillType billType;
+}
---
+++ src/main/java/com/fu/lhm/contract/model/CreateContractRequest.java
@@ -0,0 +1,28 @@
+package com.fu.lhm.contract.model;
+
+import com.fu.lhm.tenant.entity.Tenant;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateContractRequest {
+    //contract
+    @NotNull
+    private long roomId;
+    private Date fromDate;
+    private Date toDate;
+    private Long deposit;
+    private Integer autoBillDate;
+
+    //tenant
+    private Tenant tenant;
+
+}
